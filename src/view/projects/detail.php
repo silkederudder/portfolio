a02c7a52832ee38fd289091d5cb751c60f73744b
silkederudder <silke.derudder@student.howest.be>
2018-07-26 12:00:33 +0200

    More styling, Cropped the image

--- src/view/projects/detail.php
+++ src/view/projects/detail.php
@@ -1,8 +1,8 @@
 <a class="link" href="index.php?page=overview">< All projects</a>
 <section class="section-detail">
-  <h2 class="subtitle"><?php echo $project['title'] ?></h2>
+  <h2 class="subtitle subtitle-detail"><?php echo $project['title'] ?></h2>
   <img class="project-detail-img" src="assets/images/project-img.jpg" alt="Project image of <?php echo $project['title'] ?>" width="3598" height="2303"/>
-  <div class="detail-info">
+  <article class="detail-info">
     <dl class="info-list">
       <dt class="term">Team</dt>
       <dd class="text definition"><?php echo $project['team'] ?></dd>
@@ -13,17 +13,17 @@
       <dt class="term">In action</dt>
       <dd class="text definition"><a class="link" target="_blank" href="<?php echo $project['url'] ?>">Check it out</a></dd>
     </dl>
-  </div>
-  <div class="brief">
+  </article>
+  <article class="brief">
     <h3 class="heading">Brief</h3>
     <p class="text"><?php echo $project['brief'] ?></p>
-  </div>
-  </div>
-  <div class="concept">
+  </article>
+</article>
+  <article class="concept">
   <h3 class="heading">Progress &amp; concept</h3>
   <p class="text"><?php echo $project['concept'] ?></p>
-  </div>
-  <div class="visuals">
+</article>
+  <article class="visuals">
     <h3 class="heading">Visuals</h3>
-  </div>
+  </article>
 </section>
